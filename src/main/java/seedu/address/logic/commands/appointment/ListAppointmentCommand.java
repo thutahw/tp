7fe6e86ef40a2906096afe9611014e8793908a0d
jianhandev <jianhanli.dev@gmail.com>
2020-10-12 21:16:56 +0800

    Add ListAppointmentCommand class

--- src/main/java/seedu/address/logic/commands/appointment/ListAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/appointment/ListAppointmentCommand.java
@@ -1,4 +1,25 @@
 package seedu.address.logic.commands.appointment;
 
-public class ListAppointmentCommand {
+import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_APPOINTMENTS;
+
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.model.Model;
+
+/**
+ * Lists all patients in the appointment book to the user.
+ */
+public class ListAppointmentCommand extends AppointmentCommand {
+
+    public static final String COMMAND_WORD = "list";
+
+    public static final String MESSAGE_SUCCESS = "Listed all appointments";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredAppointmentList(PREDICATE_SHOW_ALL_APPOINTMENTS);
+        return new CommandResult(MESSAGE_SUCCESS, TAB_NUMBER);
+    }
 }
