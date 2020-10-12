6ef32103fe95e5dffd3fdaf638aa426df21d8db3
jianhandev <jianhanli.dev@gmail.com>
2020-10-12 21:49:53 +0800

    Fix lexicographical ordering of import statements

--- src/test/java/seedu/address/logic/parser/AppointmentBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AppointmentBookParserTest.java
@@ -13,10 +13,10 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.patient.AddPatientCommand;
-import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.patient.DeletePatientCommand;
 import seedu.address.logic.commands.patient.EditPatientCommand;
 import seedu.address.logic.commands.patient.FindPatientCommand;
