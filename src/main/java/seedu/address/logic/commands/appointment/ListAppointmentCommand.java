bf7e65b7718ec42ad1d78c62040c196fe31ce912
jianhandev <jianhanli.dev@gmail.com>
2020-10-12 21:19:32 +0800

    Change list appointment command word

--- src/main/java/seedu/address/logic/commands/appointment/ListAppointmentCommand.java
+++ src/main/java/seedu/address/logic/commands/appointment/ListAppointmentCommand.java
@@ -11,7 +11,7 @@ import seedu.address.model.Model;
  */
 public class ListAppointmentCommand extends AppointmentCommand {
 
-    public static final String COMMAND_WORD = "list";
+    public static final String COMMAND_WORD = "listAppt";
 
     public static final String MESSAGE_SUCCESS = "Listed all appointments";
 
